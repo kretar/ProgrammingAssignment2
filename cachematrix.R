85bef21c70a1ad75a2f680bff506d94a12350b89
Roald Bankras <roald@kretar.com>
2015-02-18 20:31:48 +0100

    Correctie op commentaar

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a programming assignment for the Coursera Programming in R course.
+## For the third week, the assignment is to create two method with which
+## you can calculate the inverse of a matrix and cache the result for 
+## future reference. If the inverse is requested again at a later point,
+## the result should be taken from cache.
 
 ## This is a simple method that returns a list with functions for cached inverse matrices
 
@@ -18,7 +21,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the matrix 
+## by solving it agains the identity matrix.
+## If the inverse has been calculated before, it will return the previously calculated result
 
 cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
